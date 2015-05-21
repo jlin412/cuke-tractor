21f9d9777265f95a93e3241c0d1c1fb13aa64e04
Lin, Jason (CORP) <Jason.Lin@adp.com>
2015-05-21 11:20:33 -0400

    new scenarios

--- features/some.feature
+++ features/some.feature
@@ -2,11 +2,6 @@ Feature: Some feature
 
   Scenario: failed by protactor invalid path
     Given I am on a angular website
-    When failed inside then call
-    Then something should not happen
-
-  Scenario: failed by protractor inside then call
-    Given I am on a angular website
     When invalid css path is executed
     Then something should not happen
 
@@ -24,3 +19,18 @@ Feature: Some feature
     Given something happened
     When something happens
     Then something should happen
+
+  Scenario: failed by protractor inside then call via callback
+    Given I am on a angular website
+    When something fails via callback in then function
+    Then something should not happen
+
+  Scenario: failed by protractor inside then call
+    Given I am on a angular website
+    When failed inside then call
+    Then something should not happen
+
+  Scenario: failed by protractor inside then call via exception
+    Given I am on a angular website
+    When something fails via exception in then function
+    Then something should not happen
--- features/step_definitions.js
+++ features/step_definitions.js
@@ -8,19 +8,18 @@ module.exports = function () {
     callback();
   });
 
-  i
-
   this.When(/^I am on a angular website$/, function (callback) {
     browser.get('http://angular.github.io/protractor/#/').then(callback);
   });
 
   this.When(/^invalid css path is executed$/, function (callback) {
-    $('css_bad_path').click();
+    $('css_bad_path').click().then(callback);
   });
 
   this.When(/^failed inside then call$/, function (callback) {
     $('css_bad_path').isPresent().then(function(isElmentPresent) {
       expect(isElmentPresent).to.equal(true);
+      callback();
     });
   });
 
@@ -32,8 +31,22 @@ module.exports = function () {
   this.When(/^something fails via exception$/, function (callback) {
     setTimeout(function () {
       throw new Error('#FAIL via exception');
-    }, 20);
+    }, 20).then(callback);
   });
+
+  this.When(/^something fails via exception in then function$/, function (callback) {
+    $('css_bad_path').isPresent().then(function(isElmentPresent) {
+      throw new Error('#FAIL via exception');
+      callback();
+    });
+  });
+
+  this.When(/^something fails via callback in then function$/, function (callback) {
+    $('css_bad_path').isPresent().then(function(isElmentPresent) {
+      callback.fail(new Error('#FAIL via callback'));
+    });
+  });
+
 
   this.Then(/^something should happen$/, function (callback) {
     callback();
